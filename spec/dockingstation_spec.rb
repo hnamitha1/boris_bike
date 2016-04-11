9e49d994637c7e0ff65465704c8fcfcf12676c07
hnamitha1 <namitha.h@gmail.com>
2016-04-11 16:03:16 +0100

    adding step 8 from boris_bike

--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -1,9 +1,12 @@
 require 'DockingStation'
 describe DockingStation do
   context "When initiate a new DockingStation object" do
+    new_docking_station = DockingStation.new
     it "class should be DockingStation" do
-      new_docking_station = DockingStation.new
       expect(new_docking_station.class).to eq DockingStation
+    end
+    it "Respond to method release_bike" do
+      expect(new_docking_station).to respond_to(:release_bike) 
     end
   end
 end
